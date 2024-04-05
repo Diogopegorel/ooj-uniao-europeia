6994c7740cf75afc48bad10b2736453e641946b7
Diogo Pegorel <diogopegorel@gmail.com>
2024-04-05 13:45:12 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+class UniaoEuropeia {
+    constructor(pais, capital, populacao) {
+        this.pais = pais;
+        this.capital = capital;
+        this.populacao = populacao;
+    }
+
+}
+
+const alemanha = new UniaoEuropeia('Alemanha', 'Berlim', 83800000);
+const austria = new UniaoEuropeia('Austria', 'Viena', 9042000);
+const belgica = new UniaoEuropeia('Belgica', 'Bruxelas', 11420163);
+const bulgaria = new UniaoEuropeia('Bulgária', 'Sofia', 6447710);
+const chequia = new UniaoEuropeia('Chéquia', 'Praga', 10827529);
+const chipre = new UniaoEuropeia('Chipre', 'Nicósia', 1189265);
+const croacia = new UniaoEuropeia('croacia', 'Zagreb', 4030358);
+const dinamarca = new UniaoEuropeia('Dinamarca', 'Copenhage', 5789957);
+const eslovaquia = new UniaoEuropeia('Eslovaquia', 'Bratislava', 5743415);
